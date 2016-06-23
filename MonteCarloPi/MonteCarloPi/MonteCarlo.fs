4889a85a3f265d0ed60af1d4286e27bf642a790c
Eric Olsson <edolsson@live.com>
2016-06-22 20:53:06 -0500

    Initial commit

---
+++ MonteCarloPi/MonteCarloPi/MonteCarlo.fs
@@ -0,0 +1,28 @@
+﻿module MonteCarlo
+
+open System
+
+let IsInSector x y =
+    x ** 2. + y ** 2. <= 1.
+
+let GenerateRandomPoint (r : Random) =
+    r.NextDouble(), r.NextDouble()
+
+let SimulateOneIteration totalInSector r =
+    let x,y = GenerateRandomPoint r
+
+    match IsInSector x y with
+    | true -> totalInSector + 1
+    | false -> totalInSector
+
+let RunSimulation iterationCount =
+    let r = new Random()
+
+    let rec RunSimulation' iterationCount runningTotal =
+        match iterationCount with
+        | 0 -> runningTotal
+        | _ -> RunSimulation' (iterationCount - 1) (SimulateOneIteration runningTotal r)
+
+    let totalInSector = RunSimulation' iterationCount 0
+
+    4. * double totalInSector / double iterationCount
---
+++ MonteCarloPi/MonteCarloPi/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open MonteCarlo
+
+[<EntryPoint>]
+let main argv = 
+    let iterations = 10000000
+    printfn "Running simulation for %d iterations" iterations
+
+    let pi = RunSimulation iterations
+
+    printfn "Pi was calculated to be %f." pi
+
+    System.Console.ReadKey() |> ignore
+
+    0 // return an integer exit code
