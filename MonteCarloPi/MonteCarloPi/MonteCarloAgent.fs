9ba164b480b40a84fb3c9a53b2fb08298458e0cc
Eric Olsson <edolsson@live.com>
2016-06-22 22:20:21 -0500

    Add parallel implementation

---
+++ MonteCarloPi/MonteCarloPi/MonteCarloAgent.fs
@@ -0,0 +1,10 @@
+﻿module MonteCarloParallel
+
+open MonteCarlo
+
+let RunSimulationParallel totalIterations =
+
+   seq { for i in 1..8 do yield async { return RunSimulation totalIterations } }
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.average
--- MonteCarloPi/MonteCarloPi/Program.fs
+++ MonteCarloPi/MonteCarloPi/Program.fs
@@ -1,13 +1,19 @@
 ﻿open MonteCarlo
+open MonteCarloParallel
 
 [<EntryPoint>]
 let main argv = 
-    let iterations = 10000000
-    printfn "Running simulation for %d iterations" iterations
+    let iterations = 100000000
 
-    let pi = RunSimulation iterations
+    printfn "Running simulation for %d iterations (no parallelism)" iterations
 
-    printfn "Pi was calculated to be %f." pi
+    printfn "Pi was calculated to be %f." <| RunSimulation iterations
+
+    System.Console.ReadKey() |> ignore
+
+    printfn "Running simulation for %d iterations (with parallelism)." iterations
+
+    printfn "Pi was calculated to be %f." <| RunSimulationParallel iterations
 
     System.Console.ReadKey() |> ignore
 
